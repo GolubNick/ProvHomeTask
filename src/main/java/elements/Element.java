022d4882ea53295e14975f4b8bfe298dc54163b9
nick.golub <nick.golub@ab-soft.net>
2017-03-15 10:07:17 +0200

    commit

--- src/main/java/elements/Element.java
+++ src/main/java/elements/Element.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.$;
 
-public class Element implements IElement{
+public abstract class Element implements IElement{
 
     String locator;
 
